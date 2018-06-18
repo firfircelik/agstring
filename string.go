5ffb87d7c857d29b2e5c21408e534a6912254a78
ferhat elmas <elmas.ferhat@gmail.com>
2018-06-18 11:18:33 +0200

    init

---
+++ string.go
@@ -0,0 +1,259 @@
+package agstring
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/mozillazg/go-unidecode"
+	"github.com/pkg/errors"
+	funk "github.com/thoas/go-funk"
+)
+
+var stripper = regexp.MustCompile("  +")
+
+// ReplaceMultispace replaces multiple spaces with one space and
+// also trims space from both ends
+func ReplaceMultispace(s string) string {
+	return strings.TrimSpace(stripper.ReplaceAllString(s, " "))
+}
+
+// FirstStr returns the first element of given list or empty string when the list is empty.
+func FirstStr(strings []string) string {
+	if len(strings) != 0 {
+		return strings[0]
+	}
+	return ""
+}
+
+// TrimSuffixes returns s without any of the provided trailing suffixes strings.
+func TrimSuffixes(s string, suffixes ...string) string {
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(s, suffix) {
+			return strings.TrimSuffix(s, suffix)
+		}
+	}
+	return s
+}
+
+// TrimPrefixesAndSpace returns a string without any of the provided leading prefixes at word
+// boundaries or spaces. See test for examples.
+func TrimPrefixesAndSpace(s string, prefixes []string) string {
+	if prefixes == nil || s == "" {
+		return s
+	}
+	trimAgain := true
+	rePre := make([]*regexp.Regexp, len(prefixes))
+	for trimAgain {
+		trimAgain = false
+		for i, prefix := range prefixes {
+			if prefix == "" {
+				continue
+			}
+			if rePre[i] == nil {
+				reE := fmt.Sprintf("^\\s*%s\\b(?P<rest>.*)", prefix)
+				rePre[i] = regexp.MustCompile(reE)
+			}
+			if matches, ok := RegexpGroups(rePre[i], strings.TrimSpace(s)); ok {
+				s = matches["rest"]
+				trimAgain = true
+			}
+		}
+	}
+	return strings.TrimSpace(s)
+}
+
+var nonAlphanumRegexp = regexp.MustCompile("[^[:alnum:]]")
+
+// RemoveNonAlnum removes non-alphanumeric characters from string
+func RemoveNonAlnum(s string) string {
+	return nonAlphanumRegexp.ReplaceAllLiteralString(s, "")
+}
+
+// ContainsAll checks if given slice contains all searched strings
+func ContainsAll(holder, searched []string) bool {
+	for _, s := range searched {
+		if !funk.ContainsString(holder, s) {
+			return false
+		}
+	}
+	return true
+}
+
+// StringContainsAll checks if given string contains all searched strings
+func StringContainsAll(holder string, searched []string) bool {
+	for _, s := range searched {
+		if !strings.Contains(holder, s) {
+			return false
+		}
+	}
+	return true
+}
+
+// ContainsAny checks if source slice contains any of given strings
+func ContainsAny(src, qs []string) bool {
+	for _, q := range qs {
+		if funk.ContainsString(src, q) {
+			return true
+		}
+	}
+	return false
+}
+
+// StringContainsAny is similar to ContainsAny but source is a string
+func StringContainsAny(s string, ls []string) bool {
+	for _, e := range ls {
+		if strings.Contains(s, e) {
+			return true
+		}
+	}
+	return false
+}
+
+// MatchesPrefixes checks if given string has a prefix from given prefix list
+func MatchesPrefixes(s string, prefixes []string) bool {
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(s, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
+// RemoveDiacritics removes diacritics from a string. If non-alphanumeric character is
+// encountered diacritics are removed from it. If removing diacritics is not possible, character
+// is removed.
+func RemoveDiacritics(s string) string { return unidecode.Unidecode(s) }
+
+// Normalize tries to remove the diacritics, removes remaining non-alphanumeric characters and
+// then changes case to lower
+func Normalize(s string) string {
+	s = RemoveDiacritics(s)
+	s = RemoveNonAlnum(s)
+	return strings.ToLower(s)
+}
+
+// EmptyIf returns empty string if given string equals to one
+// of the strings in empty list. Otherwise, given string is returned as it is.
+func EmptyIf(s string, emptyList ...string) string {
+	return ConvertIf(s, "", emptyList...)
+}
+
+// ConvertIf returns converted string if given string is one of the strings in the list
+func ConvertIf(val, converted string, list ...string) string {
+	for _, t := range list {
+		if val == t {
+			return converted
+		}
+	}
+	return val
+}
+
+// ValueIfExists returns value from map for given key if exists, else returns the given key
+func ValueIfExists(k string, m map[string]string) string {
+	v, ok := m[k]
+	if ok {
+		return v
+	}
+	return k
+}
+
+// ReplaceWholeWord replaces old into new only if old occurs as a whole word.
+func ReplaceWholeWord(s, old, replacement string) string {
+	s = " " + s + " "
+	old = " " + old + " "
+	replacement = " " + replacement + " "
+	s = strings.Replace(s, old, replacement, -1)
+	return s[1 : len(s)-1]
+}
+
+// StringIterator provides a generator of names / strings
+type StringIterator interface {
+	Get() string
+	Next() bool
+}
+
+// TrimSpace trims spaces in the given slice
+func TrimSpace(ls []string) []string {
+	for i := range ls {
+		ls[i] = strings.TrimSpace(ls[i])
+	}
+	return ls
+}
+
+// ToLower makes lowercase strings in the given slice
+func ToLower(ls []string) []string {
+	for i := range ls {
+		ls[i] = strings.ToLower(ls[i])
+	}
+	return ls
+}
+
+// Title ensures title formatting for given string
+func Title(s string) string { return strings.Title(strings.ToLower(s)) }
+
+// HasPrefix checks string has any one of given prefixes
+func HasPrefix(s string, prefixes ...string) bool {
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(s, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
+// NonEmpty filters nonempty strings from given slice
+func NonEmpty(ls []string) []string {
+	var nonempty []string
+	for _, s := range ls {
+		if s != "" {
+			nonempty = append(nonempty, s)
+		}
+	}
+	return nonempty
+}
+
+// IsEmpty checks if slice contains only empty strings
+func IsEmpty(ls []string) bool {
+	for _, s := range ls {
+		if s != "" {
+			return false
+		}
+	}
+	return true
+}
+
+// RemoveAllDiacritics removes diacritics from all strings in slice
+func RemoveAllDiacritics(ls []string) []string {
+	res := make([]string, len(ls))
+	for i, s := range ls {
+		res[i] = RemoveDiacritics(s)
+	}
+	return res
+}
+
+// SafeAtoi converts string, including empty string, to int
+func SafeAtoi(s string) (int, error) {
+	if s == "" {
+		return 0, nil
+	}
+	n, err := strconv.Atoi(s)
+	return n, errors.Wrap(err, "can't convert to int")
+}
+
+// RegexpGroups checks if regex matches to given string
+// If so, returns named groups with matches in a map
+func RegexpGroups(exp *regexp.Regexp, input string) (map[string]string, bool) {
+	if !exp.MatchString(input) {
+		return nil, false
+	}
+	match := exp.FindStringSubmatch(input)
+	result := make(map[string]string)
+	for i, name := range exp.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
+		}
+	}
+	return result, true
+}
---
+++ string_test.go
@@ -0,0 +1,259 @@
+package agstring
+
+import (
+	"reflect"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReplaceMultispace(t *testing.T) {
+	tests := []string{"   a   b   c d    ef  ", "     123   "}
+	expects := []string{"a b c d ef", "123"}
+
+	for i, tt := range tests {
+		res := ReplaceMultispace(tt)
+		if res != expects[i] {
+			t.Errorf("%d '%s': Expected %s, got %s", i, tt, expects[i], res)
+		}
+	}
+}
+
+func TestFirstStr(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected string
+	}{
+		{[]string{}, ""},
+		{[]string{"one"}, "one"},
+		{[]string{"one", "two"}, "one"},
+	}
+
+	for _, tt := range tests {
+		if res := FirstStr(tt.input); res != tt.expected {
+			t.Errorf("For input %v, expected %s, got %s", tt.input, tt.expected, res)
+		}
+	}
+}
+
+func TestRemoveDiacritics(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"ąćęłńóśźż ĄĆĘŁŃÓŚŹŻ", "acelnoszz ACELNOSZZ"},                         // Polish
+		{"čďěňřšťž ČĎĚŇŘŠŤŽ", "cdenrstz CDENRSTZ"},                             // Czech
+		{"éàèùâêîôûëïöüÿç ÉÀÈÙÂÊÎÔÛËÏÖÜŸÇ", "eaeuaeioueiouyc EAEUAEIOUEIOUYC"}, // French
+		{"ğöüçş ĞÖÜÇŞ", "goucs GOUCS"},                                         // Turkish
+	}
+
+	for _, tt := range tests {
+		if res := RemoveDiacritics(tt.input); res != tt.expected {
+			t.Errorf("For input %v, expected %s, got %s", tt.input, tt.expected, res)
+		}
+	}
+}
+
+func TestEmptyIf(t *testing.T) {
+	tests := []struct {
+		s         string
+		emptyList []string
+		expected  string
+	}{
+		{"", nil, ""},
+		{"agflow", nil, "agflow"},
+		{"agflow", []string{}, "agflow"},
+		{"-", []string{"-"}, ""},
+		{"?", []string{"-", "tbd"}, "?"},
+		{"agflow", []string{""}, "agflow"},
+	}
+	for i, tt := range tests {
+		if res := EmptyIf(tt.s, tt.emptyList...); res != tt.expected {
+			t.Errorf("%d: For input %q with empty list %v, expected %q, got %q",
+				i, tt.s, tt.emptyList, tt.expected, res)
+		}
+	}
+}
+
+func TestReplaceWholeWords(t *testing.T) {
+	tests := []struct {
+		s           string
+		old         string
+		replacement string
+		expected    string
+	}{
+		{"port saint jose", "nt", "nntt", "port saint jose"},
+		{"po saint jose", "po", "port", "port saint jose"},
+		{"port st jose", "st", "saint", "port saint jose"},
+		{"port saint hose", "hose", "jose", "port saint jose"},
+	}
+	for i, tt := range tests {
+		if res := ReplaceWholeWord(tt.s, tt.old, tt.replacement); res != tt.expected {
+			t.Errorf("%d: For input %q replacing %s to %s, expected %q, got %q",
+				i, tt.s, tt.old, tt.replacement, tt.expected, res)
+		}
+	}
+}
+
+func TestTrimSpace(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected []string
+	}{
+		{nil, nil},
+		{[]string{}, []string{}},
+		{[]string{"a"}, []string{"a"}},
+		{[]string{"a "}, []string{"a"}},
+		{[]string{" a "}, []string{"a"}},
+		{[]string{" a ", "b"}, []string{"a", "b"}},
+		{[]string{" a", "b "}, []string{"a", "b"}},
+	}
+	for i, tt := range tests {
+		if res := TrimSpace(tt.input); !reflect.DeepEqual(res, tt.expected) {
+			t.Errorf("%d: For input %v, expected %v, not %v", i, tt.input, tt.expected, res)
+		}
+	}
+}
+
+func TestTitle(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"abc abc", "Abc Abc"},
+		{"ABC ABC", "Abc Abc"},
+		{"Abc Abc", "Abc Abc"},
+		{"ABc aBC", "Abc Abc"},
+	}
+	for i, tt := range tests {
+		if res := Title(tt.input); res != tt.expected {
+			t.Errorf("%d: For input %v, expected %v, not %v", i, tt.input, tt.expected, res)
+		}
+	}
+}
+
+func TestSafeAtoi(t *testing.T) {
+	// Given
+	s1 := "100"
+	// When
+	i1, err1 := SafeAtoi(s1)
+	// Then
+	require.Equal(t, 100, i1)
+	require.Nil(t, err1)
+
+	// Given
+	s2 := ""
+	// When
+	i2, err2 := SafeAtoi(s2)
+	// Then
+	require.Equal(t, 0, i2)
+	require.Nil(t, err2)
+
+	// Given
+	s3 := "hello"
+	// When
+	_, err3 := SafeAtoi(s3)
+	// Then
+	require.NotNil(t, err3)
+}
+
+func TestContainsAll(t *testing.T) {
+	testCases := []struct {
+		holder, searched []string
+		expected         bool
+	}{
+		{nil, nil, true},
+		{[]string{}, []string{}, true},
+		{nil, []string{"a"}, false},
+		{[]string{"a"}, []string{"a"}, true},
+		{[]string{"a", "b"}, []string{"a"}, true},
+		{[]string{"a", "b"}, []string{"a", "c"}, false},
+	}
+
+	for _, testCase := range testCases {
+		require.Equalf(t,
+			testCase.expected,
+			ContainsAll(testCase.holder, testCase.searched),
+			"%v is searched in %v and fails")
+	}
+}
+
+func TestStringContainsAll(t *testing.T) {
+	testCases := []struct {
+		holder   string
+		searched []string
+		expected bool
+	}{
+		{"", nil, true},
+		{"", []string{}, true},
+		{"", []string{"a"}, false},
+		{"a", []string{"a"}, true},
+		{"ab", []string{"a"}, true},
+		{"ab", []string{"a", "c"}, false},
+	}
+
+	for _, testCase := range testCases {
+		require.Equalf(t,
+			testCase.expected,
+			StringContainsAll(testCase.holder, testCase.searched),
+			"%v is searched in %q and fails")
+	}
+}
+
+func TestRegexpGroups(t *testing.T) {
+	testCases := []struct {
+		re       *regexp.Regexp
+		input    string
+		matches  bool
+		expected map[string]string
+	}{
+		{
+			re:       regexp.MustCompile(`^(?P<month>[a-zA-Z]+?)\s*(?P<year>\d\d(?:\d\d)?)?$`),
+			input:    "Mrch19",
+			matches:  true,
+			expected: map[string]string{"month": "Mrch", "year": "19"},
+		},
+		{
+			re:      regexp.MustCompile(`^(?P<month>[a-zA-Z]+?)\s*(?P<year>\d\d(?:\d\d)?)?$`),
+			input:   "Mrch199",
+			matches: false,
+		},
+		{
+			re:       regexp.MustCompile(`^([a-zA-Z]+?)\s*(?P<year>\d\d(?:\d\d)?)?$`),
+			input:    "Mrch19",
+			matches:  true,
+			expected: map[string]string{"year": "19"},
+		},
+	}
+	for _, testCase := range testCases {
+		match, ok := RegexpGroups(testCase.re, testCase.input)
+		require.Equal(t, testCase.matches, ok, "for input %s", testCase.input)
+		if testCase.matches {
+			require.Equal(t, testCase.expected, match)
+		}
+	}
+}
+
+func TestTrimPrefixesAndSpace(t *testing.T) {
+	testCases := []struct {
+		input    string
+		prefixes []string
+		expected string
+	}{
+		{" cif fob massive port", []string{"fob", "cif"}, "massive port"},
+		{" cif fobmassive port", []string{"fob", "cif"}, "fobmassive port"},
+		{" cif fob massive port", []string{"big", "location"}, "cif fob massive port"},
+		{" cif fob massive port", []string{""}, "cif fob massive port"},
+		{" cif fob", []string{"fob", "cif"}, ""},
+		{" ", []string{}, ""},
+		{"", nil, ""},
+	}
+
+	for _, testCase := range testCases {
+		require.Equal(t,
+			testCase.expected,
+			TrimPrefixesAndSpace(testCase.input, testCase.prefixes),
+		)
+	}
+}
